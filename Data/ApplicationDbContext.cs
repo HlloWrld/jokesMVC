1aec3b6694ec4fb001e2a827feb4f8d84e12ae8c
hllowrld <ira.bcit@protonmail.com>
2022-02-13 14:31:56 -0800

    inital application setup

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using midtermPrepDotnetCLI.Models;
+
+namespace midtermPrepDotnetCLI.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+    public DbSet<midtermPrepDotnetCLI.Models.Joke> Joke { get; set; }
+}
